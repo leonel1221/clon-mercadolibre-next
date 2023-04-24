c1fd65b3f6b43931d639ed93ec11c14ef07781b9
DESKTOP-KFJE4B7\PC <charlstore@gmail.com>
2023-04-23 22:30:40 -0500

    ajustes finales

--- src/components/Ofertas.tsx
+++ src/components/Ofertas.tsx
@@ -9,73 +9,73 @@ const Ofertas = () => {
             <div>
                 <div className="barra-ofertas-inter" >
                     <div className="contenedor-ofertas">
-                        <img src="media\silla.svg" />                        
-                        <div className="contenedor-descuentos">                           
+                        <img src="media\silla.svg" />
+                        <div className="contenedor-descuentos">
                             <div className="conten-precio-microfono">
                                 <div className="text-precio-micro">
-                                        <span className="estilo-precios-descuento">$ 210.990</span>
+                                    <span className="estilo-precios-descuento">$ 210.990</span>
                                     <span className="descuesto-off">32% OFF</span>
                                 </div>
                                 <div className="enviogratis-full">
-                                    <span className="text-eviomicro">Envío gratis</span>                                   
-                                </div>                                
+                                    <span className="text-eviomicro">Envío gratis</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="contenedor-ofertas">
-                        <img src="media\microfono.svg" />                        
-                        <div className="contenedor-descuentos">                           
+                        <img src="media\microfono.svg" />
+                        <div className="contenedor-descuentos">
                             <div className="conten-precio-microfono">
                                 <div className="text-precio-micro">
-                                        <span className="estilo-precios-descuento">$ 74.990</span>
+                                    <span className="estilo-precios-descuento">$ 74.990</span>
                                     <span className="descuesto-off">25% OFF</span>
                                 </div>
                                 <div className="enviogratis-full">
                                     <span className="text-eviomicro">Envío gratis</span>
-                                    <img src="media\fullmicro.svg" />                                    
-                                </div>                                
+                                    <img src="media\fullmicro.svg" />
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="contenedor-ofertas">
-                        <img src="media\afeitadora.svg" />                        
-                        <div className="contenedor-descuentos">                           
+                        <img src="media\afeitadora.svg" />
+                        <div className="contenedor-descuentos">
                             <div className="conten-precio-microfono">
                                 <div className="text-precio-micro">
-                                        <span className="estilo-precios-descuento">$ 134.950</span>
+                                    <span className="estilo-precios-descuento">$ 134.950</span>
                                     <span className="descuesto-off">50% OFF</span>
                                 </div>
                                 <div className="enviogratis-full">
-                                    <span className="text-eviomicro">Envío gratis</span>                                  
-                                </div>                                
+                                    <span className="text-eviomicro">Envío gratis</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="contenedor-ofertas">
-                        <img src="media\image\armario.svg" />                        
-                        <div className="contenedor-descuentos">                           
+                        <img src="media\image\armario.svg" />
+                        <div className="contenedor-descuentos">
                             <div className="conten-precio-microfono">
                                 <div className="text-precio-micro">
-                                        <span className="estilo-precios-descuento">$ 149.900</span>
+                                    <span className="estilo-precios-descuento">$ 149.900</span>
                                     <span className="descuesto-off">30% OFF</span>
                                 </div>
                                 <div className="enviogratis-full">
-                                    <span className="text-eviomicro">Envío gratis</span>                                   
-                                </div>                                
+                                    <span className="text-eviomicro">Envío gratis</span>
+                                </div>
                             </div>
                         </div>
                     </div>
                     <div className="contenedor-ofertas">
-                        <img src="media\cortinas.svg" />                        
-                        <div className="contenedor-descuentos">                           
+                        <img src="media\cortinas.svg" />
+                        <div className="contenedor-descuentos">
                             <div className="conten-precio-microfono">
                                 <div className="text-precio-micro">
-                                        <span className="estilo-precios-descuento">$ 69.990</span>
+                                    <span className="estilo-precios-descuento">$ 69.990</span>
                                     <span className="descuesto-off">17% OFF</span>
                                 </div>
                                 <div className="enviogratis-full">
-                                    <img src="media\fullmicro.svg" />                                    
-                                </div>                                
+                                    <img src="media\fullmicro.svg" />
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -86,4 +86,4 @@ const Ofertas = () => {
     )
 }
 
-export { Ofertas};
+export { Ofertas };
